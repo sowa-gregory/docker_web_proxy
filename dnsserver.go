bff5fd26e160106964ba3b8423fec403ea756f79
Grzesiek <sowa.gregory@gmail.com>
2021-01-15 21:28:45 +0100

    update

--- dnsserver.go
+++ dnsserver.go
@@ -3,54 +3,61 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"strconv"
+
 	"github.com/miekg/dns"
 )
 
-var records = map[string]string{
-	"test.eniac.": "5.5.5.5",
+var ip string
+
+const domain = "eniac." // domain name to response for
+const port = 53
+
+func getOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
 }
 
 func parseQuery(m *dns.Msg) {
 	for _, q := range m.Question {
 		switch q.Qtype {
 		case dns.TypeA:
-			log.Printf("Query for %s\n", q.Name)
-			ip := records[q.Name]
-			if ip != "" {
-				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
-				if err == nil {
-					m.Answer = append(m.Answer, rr)
-				}
+			log.Printf("Query for %s, response:%s\n", q.Name, ip)
+			rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+			if err == nil {
+				m.Answer = append(m.Answer, rr)
 			}
 		}
 	}
 }
 
-func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
-	log.Printf("asdfasdf\n")
+func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
 	m := new(dns.Msg)
 	m.SetReply(r)
 	m.Compress = false
-
-	log.Printf("que\n")
 	switch r.Opcode {
 	case dns.OpcodeQuery:
 		parseQuery(m)
 	}
-
 	w.WriteMsg(m)
 }
 
 func main() {
+	ip = getOutboundIP()
+	log.Printf("Local ip: %s\n", ip)
 	// attach request handler func
-	dns.HandleFunc("eniac.", handleDnsRequest)
+	dns.HandleFunc(domain, handleDNSRequest)
 
 	// start server
-	port := 53
 	server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
-	log.Printf("Starting at %d\n", port)
-	log.Printf("asdfasdf\n")
+	log.Printf("Starting at port:%d\n", port)
+	log.Printf("Resolves DNS for domain:%s\n", domain)
 	err := server.ListenAndServe()
 	defer server.Shutdown()
 	if err != nil {
@@ -58,4 +65,3 @@ func main() {
 	}
 	log.Printf("running\n")
 }
-
